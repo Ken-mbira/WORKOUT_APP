b614418566efdf5aea42605cbaa48a097f4d0628
ken <ken.mbira@student.moringaschool.com>
2022-04-28 16:12:20 +0300

    add landing page format and functionality

--- app/src/main/java/devmbira/mobile/a7minutesworkout/MainActivity.kt
+++ app/src/main/java/devmbira/mobile/a7minutesworkout/MainActivity.kt
@@ -2,10 +2,17 @@ package devmbira.mobile.a7minutesworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val flStartButton : FrameLayout = findViewById(R.id.flStart)
+        flStartButton.setOnClickListener {
+            Toast.makeText(this@MainActivity,"We will start the exercise here",Toast.LENGTH_LONG).show()
+        }
     }
 }
