7b6ebaad06d8e58fe13cf6f3d660f555a0703675
ken <ken.mbira@student.moringaschool.com>
2022-04-28 15:34:49 +0300

    Initial commit

---
+++ app/src/main/java/devmbira/mobile/a7minutesworkout/MainActivity.kt
@@ -0,0 +1,11 @@
+package devmbira.mobile.a7minutesworkout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
