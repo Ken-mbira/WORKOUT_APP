25f807242cd89ef608e151ae0ca2a860020cab24
ken <ken.mbira@student.moringaschool.com>
2022-04-29 09:35:37 +0300

    implement view bindign

--- app/src/main/java/devmbira/mobile/a7minutesworkout/MainActivity.kt
+++ app/src/main/java/devmbira/mobile/a7minutesworkout/MainActivity.kt
@@ -4,15 +4,23 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.FrameLayout
 import android.widget.Toast
+import devmbira.mobile.a7minutesworkout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var binding: ActivityMainBinding? =  null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
 
-        val flStartButton : FrameLayout = findViewById(R.id.flStart)
-        flStartButton.setOnClickListener {
+//        val flStartButton : FrameLayout = findViewById(R.id.flStart)
+        binding?.flStart?.setOnClickListener {
             Toast.makeText(this@MainActivity,"We will start the exercise here",Toast.LENGTH_LONG).show()
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
     }
 }
